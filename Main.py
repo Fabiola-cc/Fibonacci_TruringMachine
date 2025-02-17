f5e4b891830458bdd25aa0456542b20213ac7a4a
Fabiola Contreras <fcontrerascol@gmail.com>
2025-02-17 12:50:46 -0600

    feat: handle 'infinite' tape

--- Main.py
+++ Main.py
@@ -1,10 +1,22 @@
 import json
+machine_file = "Fibonacci_machine.json"
+# Abrir y leer el archivo JSON
+with open(machine_file, "r", encoding="utf-8") as file:
+    data = json.load(file)  # Carga el contenido del archivo como un diccionario
+
+Q = data["states"] #Estados
+sigma = data["alphabet"] #Alfabeto de entrada
+gamma = data["tapeAlphabet"] #Alfabeto de cinta
+q0 = data["initialState"] #Estado inicial
+F = data["finalState"] #Estado final
+Blanc = data["Blanc"] #Símbolo blanco
+delta = data["delta"] #Lista de transiciones //'estado, lectura': [transicion, input, movimiento]
+tapeInput = data["tapeInput"] #Input inicial en cadena
 
 # Función para encontrar los cambios en la cinta de la máquina
-def delta_transitions(state, char_input, delta):
+def delta_transitions(state, char_input):
     key_value = f"{state}, {char_input}"
     changes = delta.get(key_value, []).copy()
-    
     if len(changes) == 0:
         return changes
 
@@ -22,20 +34,6 @@ def delta_transitions(state, char_input, delta):
 print("Bienvenido al simulador de máquina de turing")
 
 print("\nLeyendo información de la máquina")
-
-machine_file = "Fibonacci_machine.json"
-# Abrir y leer el archivo JSON
-with open(machine_file, "r", encoding="utf-8") as file:
-    data = json.load(file)  # Carga el contenido del archivo como un diccionario
-
-Q = data["states"] #Estados
-sigma = data["alphabet"] #Alfabeto de entrada
-gamma = data["tapeAlphabet"] #Alfabeto de cinta
-q0 = data["initialState"] #Estado inicial
-F = data["finalState"] #Estado final
-Blanc = data["Blanc"] #Símbolo blanco
-delta = data["delta"] #Lista de transiciones //'estado, lectura': [transicion, input, movimiento]
-tapeInput = data["tapeInput"] #Input inicial en cadena
 
 # FUNCIONAMIENTO DE MÁQUINA
 tape = [Blanc, Blanc]
@@ -56,10 +54,12 @@ index = 2 #punto de inicio de la cinta
 char_tape = tape[index] #caracter leido actualmente en la cinta
 
 while not (state == F and char_tape == Blanc):
-    changes = delta_transitions(state, char_tape, delta)
+    changes = delta_transitions(state, char_tape)
     state = changes[0]
     tape[index] = changes[1]
     index += int(changes[2])
+    if index == len(tape) - 1:
+        tape.extend([Blanc])
     char_tape = tape[index]
 
 tape_result = "".join(tape)
